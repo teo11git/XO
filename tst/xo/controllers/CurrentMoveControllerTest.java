298ee3e4d9de6c2442ad1f8a0556b3d25f6fac81
TeoGol <teobox11@gmail.com>
2020-10-15 18:32:38 +0300

    continue wrine CMC test

--- tst/xo/controllers/CurrentMoveControllerTest.java
+++ tst/xo/controllers/CurrentMoveControllerTest.java
@@ -26,5 +26,43 @@ public class CurrentMoveControllerTest {
 		assertEquals(actualValue, expectedValue);
 	}
 
+	@Test
+	public void ifNoCurrentWinner() {
+		Field field = new Field();
+		Point pnt1 = new Point(0 ,0);		
+		Point pnt2 = new Point(1 ,1);
+		Point pnt3 = new Point(2 ,2);	
+		Point pnt4 = new Point(1 ,2);
+		Point pnt5 = new Point(0 ,2);
+		
+		try {
+			field.setFigure(pnt1, Figure.X);
+			field.setFigure(pnt2, Figure.O);
+			field.setFigure(pnt3, Figure.X);
+			field.setFigure(pnt4, Figure.O);
+			field.setFigure(pnt5, Figure.X);
+		} catch(NoWinnerException e) {
+		
+		}
+		
+		CurrentMoveController cmc = new CurrentMoveController();
+
+		Figure expectedValue = Figure.O;
+		Figure actualValue = cmc.currentMove(field);
+
+		assertEquals(actualValue, expectedValue);
+
+		try{
+			field.setFigure(new Point(0, 2), Figure.O);
+		} catch(NoWinnerException e) {
+		
+		}
+
+		expectedValue = Figure.X;
+		actualValue = cmc.currentMove(field);
+
+		assertEquals(actualValue, expectedValue);
+	}
+
 
 }
